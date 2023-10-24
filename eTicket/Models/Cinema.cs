86853c736ff39e5f0a5a2f7817fc72892dccaa40
testacc8297979 <testacc8297979@gmail.com>
2023-10-24 11:18:40 +0200

    E-Commerce Movie-Theater Project

---
+++ eTicket/Models/Cinema.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTicket.Models
+{
+    public class Cinema
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Logo")]
+        public string Logo { get; set; }
+        [Display(Name = "Name")]
+
+        public string Name { get; set; }
+        [Display(Name = "Description")]
+
+        public string Description { get; set; }
+
+        //Relationships
+
+        public List<Movie> Movies { get; set; }
+    }
+}
---
+++ eTicket/Program.cs
@@ -0,0 +1,41 @@
+using eTicket.Data;
+using eTicket.Data.Services;
+using Microsoft.EntityFrameworkCore;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+
+builder.Services.AddScoped<IActorsService, ActorsService>();
+builder.Services.AddScoped<IProducersService, ProducersService>(); 
+
+
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//Seed Database
+AppDbInitializer.Seed(app);
+
+app.Run();
